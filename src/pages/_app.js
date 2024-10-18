3f367ff29abf121265ebfc5059f0eb96a3cb57b4
Prasad-Boye <boyaprasad05@outlook.com>
2024-10-18 19:11:11 +0530

    SEO compliance

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -15,6 +15,19 @@ return (
   <>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Prasad Boye | Full Stack Developer Portfolio</title>
+
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Full-stack web application developed using modern technologies like Next.js, React.js, Python, Node.js, and more. Focus on performance, UX, and scalable systems." />
+      <meta name="keywords" content="React, Next.js, Python, Node.js, Full Stack, JavaScript, Tailwind CSS, Material UI, Docker, AWS, Oracle, SQLite, Jira, Jenkins" />
+      <meta name="author" content="Prasad Boye" />
+
+      <meta property="og:title" content="Prasad Boye | Full Stack Developer Portfolio" />
+      <meta property="og:description" content="Portfolio showcasing expertise in modern web technologies, including React.js, Next.js, Python, Node.js, and more. Focused on delivering high-performance, user-friendly applications." />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://www.linkedin.com/in/prasad-boye/" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
     <main className="font-mont bg-light w-full min-h-screen">
