f73a31462006924c269f9dc72c4bb20a87b11619
Prasad-Boye <boyaprasad05@outlook.com>
2024-10-18 19:15:35 +0530

    SEO compliance

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -21,7 +21,7 @@ return (
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="description" content="Full-stack web application developed using modern technologies like Next.js, React.js, Python, Node.js, and more. Focus on performance, UX, and scalable systems." />
-      <meta name="keywords" content="React, Next.js, Python, Node.js, Full Stack, JavaScript, Tailwind CSS, Material UI, Docker, AWS, Oracle, SQLite, Jira, Jenkins" />
+      <meta name="keywords" content="React developer, Next.js developer, JavaScript developer, Front-end developer, Full Stack developer, Prasad Boye, web development, React, Next.js, Python, Node.js, Full Stack, JavaScript, Tailwind CSS, Material UI, Docker, AWS, Oracle, SQLite, Jira, Jenkins" />
       <meta name="author" content="Prasad Boye" />
 
       <meta property="og:title" content="Prasad Boye | Full Stack Developer Portfolio" />
